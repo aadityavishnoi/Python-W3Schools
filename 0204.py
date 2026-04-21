d49d44b1081606126b5b39614b630cb44cd48b85
aadityavishnoi <vishnoiaaditya29@gmail.com>
2026-04-21 13:22:15 +0530

    updated

--- 0204.py
+++
@@ -1,9 +0,0 @@
-A = {1, 2, 3, 4}
-B = {3, 4, 5, 6}
-A.add(5)
-print("Set A: ", A)
-print("Set B: ", B)
-
-print("Union:", A|B)
-print("Intersection:", A & B)
-print("Difference:", A-B)
--- dictionary.pyt
+++
@@ -1,4 +0,0 @@
-student = {"Name":"Ravi","Age":17, "Course":"Btech"}
-print(student.values())
-print(student.keys())
-print("Student Dictionary: ", student)
