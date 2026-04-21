9d56603bb55dc2ffd3da48e7865f29e2e0b15515
aadityavishnoi <vishnoiaaditya29@gmail.com>
2026-04-21 14:15:13 +0530

    commit

---
+++ MidTerm 2026/Python Looping Statements/P4.1.py
@@ -0,0 +1,20 @@
+# Program using for loop
+
+for i in range(1, 6):
+    print(i)
+# 1
+# 2
+# 3
+# 4
+# 5
+
+for i in range(1, 6, 2):
+    print(i)
+
+# 1
+# 3
+# 5
+sum = 0
+for i in range(1, 6):
+    sum += i
+print(sum)
